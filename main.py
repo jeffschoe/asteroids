ad0e8c8be31306e3981c7a27fc874dd87fa8fe65
jeffschoe <ratacame@protonmail.com>
2025-03-17 22:23:55 -0400

    Imported and initialized pygame. Created GUIi windwow screen variable. Code to check for user closing the screen. Added .gitignore for venv and pycache.

--- main.py
+++ main.py
@@ -2,11 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from constants import *
 
 
 def main():
+    pygame.init()
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #get a new GUI window:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #game loop
+    while True:
+        #This will check if the user has closed the window 
+        #and exit the game loop if they do. It will make the 
+        #window's close button work.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0,0,0)) #fill the screen with a solid "black" color
+        pygame.display.flip() #refresh the screen.
 
 
 
