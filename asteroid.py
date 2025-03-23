d450ea939f09de699934b367c14f121c0fe127ad
jeffschoe <ratacame@protonmail.com>
2025-03-22 22:10:15 -0400

    Cleaned up code.

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,13 @@
-from circleshape import *
-#from constants import *
+import pygame
+from circleshape import CircleShape
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
     
     def draw(self, screen):
-        pygame.draw.circle(screen,(255, 255, 255),self.position,self.radius,2)
-        #need a return statement?
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     
     def update(self, dt):
-        self.position += (self.velocity * dt)
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,56 +1,45 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
-from asteroidfield import *
-
-
-asteroids = pygame.sprite.Group()
-updatable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
-Asteroid.containers = (asteroids, updatable, drawable)
-Player.containers = (updatable, drawable)
-AsteroidField.containers = (updatable)
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    #get a new GUI window:
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #get a new GUI window:
     clock = pygame.time.Clock() #create an object to help track time
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #create player in middle of screen
+
     dt = 0 #delta time
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS) #create player in middle of screen
-    asteroidfield = AsteroidField() # create asteroid field
-
-    #game loop
     while True:
         #This will check if the user has closed the window 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
-
-        #player.update(dt)
         updatable.update(dt)
-        screen.fill((0,0,0)) #fill the screen with a solid "black" color
-        #player.draw(screen)
-        for thing in drawable:
-            thing.draw(screen)
-
         
+        screen.fill("black") #fill the screen with a solid "black" color
+    
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip() #refresh the screen.
-        dt = clock.tick(60)/1000 #Pause the game loop until 1/60th of a second has passed.
-        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000 #Pause the game loop until 1/60th of a second has passed.        
 
 
 if __name__ == "__main__":
